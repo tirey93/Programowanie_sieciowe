17852e68f974d3361bad6bcca4d71724f504f899
tirey93 <tirey93@gmail.com>
2018-11-25 12:40:42 +0100

    zaliczenie

--- sieciowe1/src/sieciowe1/ClientUDP.java
+++ sieciowe1/src/sieciowe1/ClientUDP.java
@@ -1,37 +1,79 @@
 package sieciowe1;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class ClientUDP {
 	public static Scanner scanner = null;
 	public static void main(String[] args) throws Exception{
-        DatagramSocket s = new DatagramSocket();
-        String sentence = "";
-        byte[] message = "Test".getBytes("utf8");
-
+		MulticastSocket clientSocketSend = new MulticastSocket(3301);
+		MulticastSocket clientSocketReceive = new MulticastSocket(3301);
+		InetAddress group = InetAddress.getByName("239.255.42.99");
+		clientSocketSend.joinGroup(group);
+        String sentence = "";  
+        String name = "";
+        String myName = "";
+        //clientSocket.setLoopbackMode(true);
+        clientSocketSend.setReuseAddress(true);
+        clientSocketSend.setSoTimeout(1000);
         
-        DatagramPacket packet = new DatagramPacket(message, message.length);
-        packet.setPort(3301);
-        packet.setAddress(InetAddress.getByName("239.255.42.99"));
-
-        s.setSoTimeout(1000);
+        clientSocketReceive.joinGroup(group);
+        //clientSocket.setLoopbackMode(true);
+        clientSocketReceive.setReuseAddress(true);
+        clientSocketReceive.setSoTimeout(1000);
+        
         scanner = new Scanner(System.in);
+        DatagramPacket packet;
+		myName = setNick(clientSocketSend, group);
+        Thread.sleep(1000);
+        DatagramPacket response = new DatagramPacket(new byte[1000], 1000);
+        try{
+        	clientSocketReceive.receive(response);
+        	String message = new String(response.getData(), 0,  response.getLength(), "utf8");
+        	System.out.println("X: " + message);
+        	clientSocketReceive.receive(response);
+        	while(message.contains("Busy")) {
+        		System.out.println("Choose another nick: ");
+        		myName = setNick(clientSocketSend, group);
+        	}
+		}catch (IOException e){
+	        //System.out.println("Nie otrzyma≥em odpowiedzi");
+	    }
+        System.out.println("Nick is registered.");
+        ClientUDPThread cut = new ClientUDPThread(clientSocketReceive, clientSocketSend, myName);
+        cut.isThreadBusy = true;
+        Thread th = new Thread(cut);
+        th.start();
         while(!(sentence = scanner.nextLine()).equals("quit")) {
-			packet.setData(sentence.getBytes());
-	        s.send(packet);
-	        DatagramPacket response = new DatagramPacket(new byte[1000], 1000);
-	        try{
-	            s.receive(response);
-	            System.out.println("Odpowiedü: ");
-	            System.out.println(new String(response.getData(), 0,  response.getLength(), "utf8"));
-	        }catch (SocketTimeoutException e){
-	            System.out.println("Nie otrzyma≥em odpowiedzi");
-	        }
+        	cut.isThreadBusy = false;
+        	sentence = "MSG " + myName.substring(6) + " :" + sentence;
+        	packet = new DatagramPacket(
+        			sentence.getBytes(), 
+        			sentence.getBytes().length,
+        			group,
+        			3301);
+	        clientSocketSend.send(packet);
+	        
 		}
 
     }
+	private static String setNick(MulticastSocket clientSocketSend, InetAddress group) throws IOException {
+		String myName;
+		System.out.println("Choose nick: ");
+
+        myName = scanner.nextLine();
+        myName = "NICK: " + myName;
+        DatagramPacket packet = new DatagramPacket(
+        		myName.getBytes(), 
+        		myName.getBytes().length,
+    			group,
+    			3301);
+        clientSocketSend.send(packet);
+		return myName;
+	}
 }
--- sieciowe1/src/sieciowe1/ServerUDP.java
+++ sieciowe1/src/sieciowe1/ServerUDP.java
@@ -11,24 +11,28 @@ public class ServerUDP {
 	public static void main(String[] args) throws Exception{
 
         InetAddress group = InetAddress.getByName("239.255.42.99");
-        MulticastSocket s = new MulticastSocket(3301);
-        s.joinGroup(group);
-
+        MulticastSocket serverSocket = new MulticastSocket(3301);
+        serverSocket.joinGroup(group);
+        serverSocket.setReuseAddress(true);
+        //serverSocket.setLoopbackMode(true);
         try{
             while (true){
                 DatagramPacket recv = new DatagramPacket(new byte[1000], 1000);
-                s.receive(recv);
-                String stringMsg = new String(recv.getData(), 0, recv.getLength(), "utf8");
-                System.out.println("Odebra³em wiadomoœæ: \"" + stringMsg + "\"");
-                DatagramSocket responseSocket = new DatagramSocket();
-                DatagramPacket response = new DatagramPacket(stringMsg.getBytes(), stringMsg.getBytes().length);
-                response.setAddress(recv.getAddress());
-                response.setPort(recv.getPort());
+                serverSocket.receive(recv);
+                String stringMsg = new String(recv.getData(), 0, recv.getLength(), "utf8") + "+";
+                System.out.println("Od: " +  recv.getSocketAddress() + " W: \"" + stringMsg + "\"");
+//                DatagramSocket responseSocket = new DatagramSocket();
+                DatagramPacket response = new DatagramPacket(
+                		stringMsg.getBytes(), 
+                		stringMsg.getBytes().length,
+                		group,
+                		3301);
 
-                responseSocket.send(response);
+                serverSocket.send(response);
+                Thread.sleep(1000);
             }
         }finally {
-            s.leaveGroup(group);
+            serverSocket.close();
         }
     }
 
